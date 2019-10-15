14098c4580c20cda3efec46eed7cd057f63247f1
Siddhant Bansal <si_bansa@live.concordia.ca>
2019-10-15 16:10:37 -0400

    fixed indentation and javadocs

--- src/main/java/models/Continent.java
+++ src/main/java/models/Continent.java
@@ -3,122 +3,116 @@ package models;
 import java.util.Objects;
 
 public class Continent {
-	/** The colour of the continent (serialization). */
-	private String color;
+  /** The colour of the continent (serialization). */
+  private String color;
 
-	/** The name of the continent. */
-	private String name;
+  /** The name of the continent. */
+  private String name;
 
-	/** The control value of the continent. */
-	private int value;
+  /** The control value of the continent. */
+  private int value;
 
-	/**
-	 * The constructor for a Continent object.
-	 *
-	 * @param color The colour of the continent.
-	 * @param name The name of the continent.
-	 * @param value The control value of the continent.
-	 */
-	public Continent(String color, String name, int value) {
-		this.color = color;
-		this.name = name;
-		this.value = value;
-	}
+  /**
+   * The constructor for a Continent object.
+   *
+   * @param color The colour of the continent.
+   * @param name The name of the continent.
+   * @param value The control value of the continent.
+   */
+  public Continent(String color, String name, int value) {
+    this.color = color;
+    this.name = name;
+    this.value = value;
+  }
 
-	/**
-	 * The constructor for the Continent object.
-	 *
-	 * @param name The name of the continent.
-	 * @param value The control value of the continent.
-	 */
-	public Continent(String name, int value) {
-		this.color = "BLANK";
-		this.name = name;
-		this.value = value;
-	}
+  /**
+   * The constructor for the Continent object.
+   *
+   * @param name The name of the continent.
+   * @param value The control value of the continent.
+   */
+  public Continent(String name, int value) {
+    this.color = "BLANK";
+    this.name = name;
+    this.value = value;
+  }
 
-	/**
-	 * Returns the colour.
-	 *
-	 * @return A String with the colour.
-	 */
-	public String getColor() {
-		return color;
-	}
+  /**
+   * Returns the colour.
+   *
+   * @return A String with the colour.
+   */
+  public String getColor() {
+    return color;
+  }
 
-	/**
-	 * Sets the colour.
-	 *
-	 * @param color A String with the colour.
-	 */
-	public void setColor(String color) {
-		this.color = color;
-	}
+  /**
+   * Sets the colour.
+   *
+   * @param color A String with the colour.
+   */
+  public void setColor(String color) {
+    this.color = color;
+  }
 
-	/**
-	 * Returns the name.
-	 *
-	 * @return A String with the name.
-	 */
-	public String getName() {
-		return name;
-	}
+  /**
+   * Returns the name.
+   *
+   * @return A String with the name.
+   */
+  public String getName() {
+    return name;
+  }
 
-	/**
-	 * Sets the name.
-	 *
-	 * @param name A String with the name.
-	 */
-	public void setName(String name) {
-		this.name = name;
-	}
+  /**
+   * Sets the name.
+   *
+   * @param name A String with the name.
+   */
+  public void setName(String name) {
+    this.name = name;
+  }
 
-	/**
-	 * Returns the control value.
-	 *
-	 * @return An int with the control value.
-	 */
-	public int getValue() {
-		return value;
-	}
+  /**
+   * Returns the control value.
+   *
+   * @return An int with the control value.
+   */
+  public int getValue() {
+    return value;
+  }
 
-	/**
-	 * Sets the control value.
-	 *
-	 * @param value An int with the control value.
-	 */
-	public void setValue(int value) {
-		this.value = value;
-	}
+  /**
+   * Sets the control value.
+   *
+   * @param value An int with the control value.
+   */
+  public void setValue(int value) {
+    this.value = value;
+  }
 
-	/** Pretty prints the Continent object. */
-	@Override
-	public String toString() {
-		return String.format("%s %d %s", this.name, this.value, this.color);
-	}
+  /** Pretty prints the Continent object. */
+  @Override
+  public String toString() {
+    return String.format("%s %d %s", this.name, this.value, this.color);
+  }
 
-	/**
-	 * Checks whether one Continent object is equal to another.
-	 */
-	@Override
-	public boolean equals(Object o) {
-		if (this == o) {
-			return true;
-		}
-		if (o == null || getClass() != o.getClass()) {
-			return false;
-		}
-		Continent continent = (Continent) o;
-		return value == continent.value &&
-				color.equals(continent.color) &&
-				name.equals(continent.name);
-	}
+  /** Checks whether one Continent object is equal to another. */
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    Continent continent = (Continent) o;
+    return value == continent.value && color.equals(continent.color) && name.equals(continent.name);
+  }
 
-	/**
-	 * @return int hash code for the Continent object.
-	 */
-	@Override
-	public int hashCode() {
-		return Objects.hash(color, name, value);
-	}
+  /** @return int hash code for the Continent object. */
+  @Override
+  public int hashCode() {
+    return Objects.hash(color, name, value);
+  }
 }
--- src/main/java/models/Country.java
+++ src/main/java/models/Country.java
@@ -8,231 +8,223 @@ import java.util.Objects;
  * @author Vijay
  */
 public class Country {
-	/** The name of the continent the Country belongs to. */
-	private String continent;
+  /** The name of the continent the Country belongs to. */
+  private String continent;
 
-	/** The name of the Country. */
-	private String name;
+  /** The name of the Country. */
+  private String name;
 
-	/** The x coordinate for the Country (serialization). */
-	private int x;
+  /** The x coordinate for the Country (serialization). */
+  private int x;
 
-	/** The y coordinate for the Country (serialization). */
-	private int y;
+  /** The y coordinate for the Country (serialization). */
+  private int y;
 
-	/** The name of the Player who owns the Country. */
-	private String ownerName;
+  /** The name of the Player who owns the Country. */
+  private String ownerName;
 
-	/**
-	 * Maintains the number of armies currently in a country.
-	 */
-	private int numberOfArmies;
+  /** Maintains the number of armies currently in a country. */
+  private int numberOfArmies;
 
-	/**
-	 * The constructor for the Country class.
-	 *
-	 * @param name The name of the Country.
-	 * @param continent The name of the Continent it belongs to.
-	 */
-	public Country(String name, String continent) {
-		super();
-		this.name = name;
-		this.continent = continent;
-		this.x = 0;
-		this.y = 0;
-	}
+  /**
+   * The constructor for the Country class.
+   *
+   * @param name The name of the Country.
+   * @param continent The name of the Continent it belongs to.
+   */
+  public Country(String name, String continent) {
+    super();
+    this.name = name;
+    this.continent = continent;
+    this.x = 0;
+    this.y = 0;
+  }
 
-	/**
-	 * The constructor for the Country class.
-	 *
-	 * @param name The name of the Country.
-	 * @param continent The name of the Continent it belongs to.
-	 * @param x The x coordinate of the country.
-	 * @param y The y coordinate of teh country.
-	 */
-	public Country(String name, String continent, int x, int y) {
-		super();
-		this.name = name;
-		this.continent = continent;
-		this.x = x;
-		this.y = y;
-	}
+  /**
+   * The constructor for the Country class.
+   *
+   * @param name The name of the Country.
+   * @param continent The name of the Continent it belongs to.
+   * @param x The x coordinate of the country.
+   * @param y The y coordinate of teh country.
+   */
+  public Country(String name, String continent, int x, int y) {
+    super();
+    this.name = name;
+    this.continent = continent;
+    this.x = x;
+    this.y = y;
+  }
 
-	/**
-	 * Returns the name of the continent.
-	 *
-	 * @return String with the name.
-	 */
-	public String getContinent() {
-		return this.continent;
-	}
+  /**
+   * Returns the name of the continent.
+   *
+   * @return String with the name.
+   */
+  public String getContinent() {
+    return this.continent;
+  }
 
-	/**
-	 * Sets the name of the continent
-	 *
-	 * @param continent String with the name.
-	 */
-	public void setContinent(String continent) {
-		this.continent = continent;
-	}
+  /**
+   * Sets the name of the continent
+   *
+   * @param continent String with the name.
+   */
+  public void setContinent(String continent) {
+    this.continent = continent;
+  }
 
-	/**
-	 * Returns the name of the country.
-	 *
-	 * @return String with the name.
-	 */
-	public String getName() {
-		return name;
-	}
+  /**
+   * Returns the name of the country.
+   *
+   * @return String with the name.
+   */
+  public String getName() {
+    return name;
+  }
 
-	/**
-	 * Sets the name of the country.
-	 *
-	 * @param name String with the name.
-	 */
-	public void setName(String name) {
-		this.name = name;
-	}
+  /**
+   * Sets the name of the country.
+   *
+   * @param name String with the name.
+   */
+  public void setName(String name) {
+    this.name = name;
+  }
 
-	/**
-	 * Gets the x coordinate.
-	 *
-	 * @return Integer with the x coordinate.
-	 */
-	public int getX() {
-		return this.x;
-	}
+  /**
+   * Gets the x coordinate.
+   *
+   * @return Integer with the x coordinate.
+   */
+  public int getX() {
+    return this.x;
+  }
 
-	/**
-	 * Sets the x coordinate.
-	 *
-	 * @param x An integer with the x coordinate.
-	 */
-	public void setX(int x) {
-		this.x = x;
-	}
+  /**
+   * Sets the x coordinate.
+   *
+   * @param x An integer with the x coordinate.
+   */
+  public void setX(int x) {
+    this.x = x;
+  }
 
-	/**
-	 * Gets the y coordinate.
-	 *
-	 * @return Integer with the y coordinate.
-	 */
-	public int getY() {
-		return this.y;
-	}
+  /**
+   * Gets the y coordinate.
+   *
+   * @return Integer with the y coordinate.
+   */
+  public int getY() {
+    return this.y;
+  }
 
-	/**
-	 * Sets the y coordinate.
-	 *
-	 * @param y An integer with the y coordinate.
-	 */
-	public void setY(int y) {
-		this.y = y;
-	}
+  /**
+   * Sets the y coordinate.
+   *
+   * @param y An integer with the y coordinate.
+   */
+  public void setY(int y) {
+    this.y = y;
+  }
 
-	/**
-	 * Returns the name of the owner.
-	 *
-	 * @return A String with the name.
-	 */
-	public String getOwnerName() {
-		return this.ownerName;
-	}
+  /**
+   * Returns the name of the owner.
+   *
+   * @return A String with the name.
+   */
+  public String getOwnerName() {
+    return this.ownerName;
+  }
 
-	/**
-	 * Sets the name of the owner of the country.
-	 *
-	 * @param ownerName String with the name.
-	 */
-	public void setOwnerName(String ownerName) {
-		this.ownerName = ownerName;
-	}
+  /**
+   * Sets the name of the owner of the country.
+   *
+   * @param ownerName String with the name.
+   */
+  public void setOwnerName(String ownerName) {
+    this.ownerName = ownerName;
+  }
 
-	/** Pretty prints the Country object. */
-	@Override
-	public String toString() {
-		return String.format("%s %s", this.name, this.continent);
-	}
+  /** Pretty prints the Country object. */
+  @Override
+  public String toString() {
+    return String.format("%s %s", this.name, this.continent);
+  }
 
-	/**
-	 * @return String a formatted print of countries by ownership.
-	 */
-	public String showCountryByOwnership() {
-		return String.format(
-				"%s:%s:%s:%d", this.name, this.continent, this.ownerName, this.numberOfArmies);
-	}
-	
-	/**
-	 * Returns the number of armies
-	 *
-	 * @return An integer with the number of armies
-	 */
-	public int getNumberOfArmies() {
-		return numberOfArmies;
-	}
+  /** @return String a formatted print of countries by ownership. */
+  public String showCountryByOwnership() {
+    return String.format(
+        "%s:%s:%s:%d", this.name, this.continent, this.ownerName, this.numberOfArmies);
+  }
 
-	/**
-	 * Sets the number of armies
-	 *
-	 * @param numberOfArmies number of armies
-	 */
-	public void setNumberOfArmies(int numberOfArmies) {
-		this.numberOfArmies = numberOfArmies;
-	}
+  /**
+   * Returns the number of armies
+   *
+   * @return An integer with the number of armies
+   */
+  public int getNumberOfArmies() {
+    return numberOfArmies;
+  }
 
-	/**
-	 * Adds armies to the country
-	 *
-	 * @param count an Integer to add to the current armies of a country
-	 * @return true if armies can be added
-	 */
-	public boolean addArmies(int count) {
-		if (numberOfArmies + count > 0) {
-			this.numberOfArmies += count;
-			return true;
-		}
-		return false;
-	}
+  /**
+   * Sets the number of armies
+   *
+   * @param numberOfArmies number of armies
+   */
+  public void setNumberOfArmies(int numberOfArmies) {
+    this.numberOfArmies = numberOfArmies;
+  }
 
-	/**
-	 * Removes armies from a country
-	 *
-	 * @param count an Integer to remove from the current armies of a country
-	 * @return true if armies can be removed
-	 */
-	public boolean removeArmies(int count) {
-		if (numberOfArmies - count > 0) {
-			this.numberOfArmies -= count;
-			return true;
-		}
-		return false;
-	}
+  /**
+   * Adds armies to the country
+   *
+   * @param count an Integer to add to the current armies of a country
+   * @return true if armies can be added
+   */
+  public boolean addArmies(int count) {
+    if (numberOfArmies + count > 0) {
+      this.numberOfArmies += count;
+      return true;
+    }
+    return false;
+  }
 
-	/**
-	 * Checks if a Country object is equal to another.
-	 */
-	@Override
-	public boolean equals(Object o) {
-		if (this == o) {
-			return true;
-		}
-		if (o == null || getClass() != o.getClass()) {
-			return false;
-		}
-		Country country = (Country) o;
-		return x == country.x
-				&& y == country.y
-				&& numberOfArmies == country.numberOfArmies
-				&& continent.equals(country.continent)
-				&& name.equals(country.name)
-				&& Objects.equals(ownerName, country.ownerName);
-	}
+  /**
+   * Removes armies from a country
+   *
+   * @param count an Integer to remove from the current armies of a country
+   * @return true if armies can be removed
+   */
+  public boolean removeArmies(int count) {
+    if (numberOfArmies - count > 0) {
+      this.numberOfArmies -= count;
+      return true;
+    }
+    return false;
+  }
 
-	/**
-	 * @return int hash code for the Country object.
-	 */
-	@Override
-	public int hashCode() {
-		return Objects.hash(continent, name, x, y, ownerName, numberOfArmies);
-	}
+  /** Checks if a Country object is equal to another. */
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    Country country = (Country) o;
+    return x == country.x
+        && y == country.y
+        && numberOfArmies == country.numberOfArmies
+        && continent.equals(country.continent)
+        && name.equals(country.name)
+        && Objects.equals(ownerName, country.ownerName);
+  }
+
+  /** @return int hash code for the Country object. */
+  @Override
+  public int hashCode() {
+    return Objects.hash(continent, name, x, y, ownerName, numberOfArmies);
+  }
 }
