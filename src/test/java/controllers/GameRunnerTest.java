cc3ebc57f0bded838127b1ac50a0f5589b8259b0
SabariVenkadesh <55324843+SabariVenkadesh@users.noreply.github.com>
2019-10-16 03:36:17 +0530

    Added tests for validateplayercount

--- src/test/java/controllers/GameRunnerTest.java
+++ src/test/java/controllers/GameRunnerTest.java
@@ -1,18 +1,38 @@
 package controllers;
 
-import static org.junit.Assert.*;
-
 import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 public class GameRunnerTest {
 
   @Test
   public void validatePlayerCount() {
-	  
+    String reason, reason2;
+    ArrayList<String> playersList = new ArrayList<>();
+    playersList.add("player1");
+    boolean result = GameRunner.validatePlayerCount(playersList);
+    reason = "Player count should be greater than 2";
+    assertFalse(reason, result);
+    playersList.add("player2");
+    boolean result1;
+    if (GameRunner.validatePlayerCount(playersList)) result1 = true;
+    else result1 = false;
+    assertTrue(result1);
+    playersList.add("player3");
+    playersList.add("player4");
+    playersList.add("player5");
+    playersList.add("player6");
+
+    boolean result2 = GameRunner.validatePlayerCount(playersList);
+    assertTrue(result2);
+    playersList.add("player7");
+    boolean result3 = GameRunner.validatePlayerCount(playersList);
+    reason2 = "Player count should be less than 6";
+    assertFalse(reason2, result3);
   }
 
-  @Test
-  public void populateCountries() {
-	  
-  }
 }
